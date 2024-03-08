26d41b3e62ca5e70070a87fbf9d4c8d0e22331a2
Ziad Ahmed <ziadelnady555@gmail.com>
2024-03-08 20:06:39 +0200

    Initial Commit

---
+++ Nutrex/NutrexApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutrexApp.swift
+//  Nutrex
+//
+//  Created by Ziad Ahmed on 08/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct NutrexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
