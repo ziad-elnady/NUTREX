24564c0c88369baef122ae9b0fd105803aaee5dc
Ziad Ahmed <ziadelnady555@gmail.com>
2024-03-11 15:00:21 +0200

    assets update | launch screen | app icon

---
+++ Nutrex/Features/Authentication/AuthenticatorView.swift
@@ -0,0 +1,18 @@
+//
+//  AuthenticatorView.swift
+//  Nutrex
+//
+//  Created by Ziad Ahmed on 11/03/2024.
+//
+
+import SwiftUI
+
+struct AuthenticatorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthenticatorView()
+}
